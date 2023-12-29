649b04887bc4b990eada7e801c8bd34eb90d0800
Cristian Castaño <81573985+CristianSk23@users.noreply.github.com>
2023-12-29 15:34:37 -0500

    Home Responsive Gif

--- src/components/contactMe/contact.jsx
+++ src/components/contactMe/contact.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./styles.css";
+import { Container } from "@mui/material";
 const Contact = () => {
-  return <div className="contenContact">Contact</div>;
+  return (
+    <>
+      <Container>
+        <Container id="titleContact" >
+          {" "}
+        </Container>
+        <div></div>
+        <div className="contenContact">Contact</div>
+      </Container>
+    </>
+  );
 };
 
 export default Contact;
