e9def0e7b81ec1714b0acd2fb33cced1302bd0f6
Cristian Castaño <81573985+CristianSk23@users.noreply.github.com>
2024-01-12 12:44:41 -0500

    New info and images for portfolio

--- src/components/Jobs/jobs.jsx
+++ src/components/Jobs/jobs.jsx
@@ -10,39 +10,130 @@ import {
   CardContent,
   CardActions,
   Button,
+  Grid,
 } from "@mui/material";
-
+import imageTech from "../Design/Tech/ft1.jpg";
 
 const Jobs = () => {
   return (
     <Paper>
       <Container elevation={4}>
-        <Box sx={{ height: "1080px", marginTop: "30px", paddingTop: "60px" }}>
-          <Typography variant="h3" sx={{ textAlign: "center" }}>
-            Portafolio
-          </Typography>
+        <Box
+          sx={{
+            height: "1080px",
+            marginTop: "30px",
+            paddingTop: "60px",
+            borderWidth: "60px ",
+          }}
+        >
           <Box>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+            <Typography
+              variant="subtitle2"
+              sx={{ textAlign: "center", fontSize: "2.5rem" }}
+            >
+              Portafolio
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ textAlign: "center", fontSize: "1.5rem" }}
+            >
+              Info portafolio
+            </Typography>
           </Box>
+          <Grid
+            container
+            rowSpacing={2}
+            columnSpacing={{ xs: 1, sm: 1, md: 6, lg: 5 }}
+          >
+            <Grid item xs={4}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  marginTop: "60px",
+                  width: "310px",
+                  height: "387px",
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 203 }}
+                  image={imageTech}
+                  title="TechNook"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    TechNook
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    TechNook, una plataforma E-Commerce cuidadosamente creada
+                    por un equipo de 8 desarrolladores.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Mas info</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            {/* Card Sobre TechNook */}
+            <Grid item xs={4}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  marginTop: "60px",
+                  width: "310px",
+                  height: "387px",
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 203 }}
+                  image={imageTech}
+                  title="TechNook"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Web sobre Videojuegos
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    Aplicación web para amantes de videojuegos, permite explorar
+                    y gestionar información de juegos mediante integración con
+                    la API de Rawg.io.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Mas info</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={3}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  marginTop: "60px",
+                  width: "310px",
+                  height: "387px",
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 203 }}
+                  image={imageTech}
+                  title="TechNook"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Web sobre Videojuegos
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    Aplicación web para amantes de videojuegos, permite explorar
+                    y gestionar información de juegos mediante integración con
+                    la API de Rawg.io.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Mas info</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+          {/* Card Sobre TechNook */}
         </Box>
       </Container>
     </Paper>
